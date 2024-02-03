72e7f7f0b31f0e05a28d1dc83d42e5264892189b
Ayush-projects <kayush479@gmail.com>
2024-02-03 13:01:43 +0530

    test

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3000
+const requestIp = require('request-ip');
+var geoip = require('geoip-lite');
+
+
+
+
+app.get("/", (req,res)=>{
+  let test = "103.16.69.135"
+  let ip = requestIp.getClientIp(req)
+  var geo = geoip.lookup(test);
+  res.json(geo)
+})
+
+
+
+
+app.listen(3000, ()=>{
+    console.log("Server is running on PORT", 3000)
+})
