6d27707192dddfa7fb225f34d50b1aeb35bb2b84
moonisashraf <137399872+moonisashraf@users.noreply.github.com>
2024-10-11 17:07:23 +0530

    version1 app.js

--- app.js
+++ app.js
@@ -1,25 +1,17 @@
-const express = require('express')
-const app = express()
-const PORT = process.env.PORT || 3000
-const requestIp = require('request-ip');
-var geoip = require('geoip-lite');
-var cors = require('cors')
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
 
+app.use(express.static('public'));
+app.get('/get-ip', (req, res) => {
+    const ipAddress = req.ip;
+    res.send(ipAddress);
+});
 
-app.use(cors())
+// app.get('/', (req, res)=>{
+//     res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// })
 
-
-
-app.get("/", (req,res)=>{
-  
-  let ip = requestIp.getClientIp(req)
-  var geo = geoip.lookup(ip);
-  res.json(geo)
-})
-
-
-
-
-app.listen(3000, ()=>{
-    console.log("Server is running on PORT", 3000)
-})
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
