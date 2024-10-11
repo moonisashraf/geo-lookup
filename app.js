6116719f24fd6ece5267a632fb2d8eabd4abfc12
moonisashraf <137399872+moonisashraf@users.noreply.github.com>
2024-10-11 17:14:49 +0530

    version2 app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
-app.get('/get-ip', (req, res) => {
+app.get('/', (req, res) => {
     const ipAddress = req.ip;
     res.send(ipAddress);
 });
